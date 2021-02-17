0f50b2c694a84ed5a8d6cbb1252e99d6c08bcb3a
Harman Tiwana <47931837+HarmanTiwana901@users.noreply.github.com>
2021-02-16 18:12:48 -0800

    Add files via upload

---
+++ assignment2_2208/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string word = "(((((((((((((())))))";
+            int bracketCount = 0;
+            foreach(char h in word)
+            {
+                if(h.ToString() == "(")
+                {
+                    bracketCount += 1;
+                } else if(h.ToString() == ")")
+                {
+                    bracketCount -= 1;
+                }
+            }
+
+            if (bracketCount < 0)
+            {
+                Console.WriteLine("Error: need to return a positive integer");
+            }
+            else
+            {
+                Console.WriteLine("Bracket Count: " + bracketCount);
+                
+            }
+        }
+    }
+}
