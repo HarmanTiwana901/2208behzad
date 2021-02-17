17643b5bf03cf2cced1d804cc37f55c1796f78a0
Harman Tiwana <47931837+HarmanTiwana901@users.noreply.github.com>
2021-02-16 17:58:00 -0800

    Add files via upload

---
+++ assignment1_2208/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+
+class Assignment2
+{
+
+    public bool validateIPAddress(string ip)
+    {
+        IPAddress ipParsed;
+        bool ValidateIP = IPAddress.TryParse(ip, out ipParsed);
+        if (ValidateIP)
+        {
+            Console.WriteLine("Valid ip address");
+            return true;
+        } else
+        {
+            Console.WriteLine("Invalid ip address");
+            return false;
+        }
+           
+       
+            
+        
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Assignment2 validate = new Assignment2();
+        //Console.WriteLine(validate.ValidateIPAddress("192.168.56.1"));
+
+        //var ipParts = new Byte[] { 101, 102, 103, 0 };
+        var checker = validate.validateIPAddress("192.168.56.1");
+
+        Console.WriteLine("IP Address:{0}", checker);
+       
+    }
+}
